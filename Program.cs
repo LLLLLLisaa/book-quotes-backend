d285331511eb46a800889e09781d66f3b7ac759e
LLLLLLisaa <xianochchu@gmail.com>
2026-08-25 12:39:30 +0200

    fix: apply database migrations on startup

--- Program.cs
+++ Program.cs
@@ -103,4 +103,10 @@ app.UseAuthorization();
 app.MapGet("/", () => Results.Redirect("/swagger"));
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
